49082bda36975d3c740c0554411af90b0c050453
ucokpel1112 <kristopelltoruan11@gmail.com>
2020-03-03 20:52:29 +0700

    Store eg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
 <head>
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
     <script src="{{ mix('js/app.js') }}" type="text/javascript" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body style='background-color: white'>
     <div id="app">
+    <example-component></example-component>   
     </div>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/{any}', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'Contoh@index');
